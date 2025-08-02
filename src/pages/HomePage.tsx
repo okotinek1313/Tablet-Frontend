a202c7ebda77e5c648e75dcbf80f3b6e3d0643f2
okotinek1313 <jhuotattia@gmail.com>
2025-08-02 13:02:13 -0400

    fixed minor bugs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import '../css/HomePage.css';
 import * as LucideIcon from 'lucide-react'
 import { useNavigate } from 'react-router-dom';
 
+
 export default function HomePage() {
   const [isOpen, setIsOpen] = useState(false);
   const lucideIconSize = 28;
@@ -20,10 +21,12 @@ export default function HomePage() {
         isOpen={isOpen}
         onToggle={() => setIsOpen(!isOpen)}
       >
-        <button id={"appsSidebarButton"} className={"lucideIcon"} onClick={goToApps}><LucideIcon.LayoutGrid size={lucideIconSize}></LucideIcon.LayoutGrid></button>
+        <button id={"appsSidebarButton"} className={"lucideIcon"} onClick={goToApps}><LucideIcon.LayoutGrid size={lucideIconSize} /></button>
       </Sidebar>
 
-      
+      <div className="home-page-content">
+        <button id="local-content"><LucideIcon.HardDrive size={128} /></button>
+      </div>
     </div>
   );
 }
